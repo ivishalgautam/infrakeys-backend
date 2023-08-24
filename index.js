6a3e543245487b407fa3c176f61ae66d4ea92f75
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-08-24 15:29:30 +0530

    added multiple feature creation

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/api/products", require("./router/product"));
 app.use("/api/product-applications", require("./router/product-application"));
 app.use("/api/product-descriptions", require("./router/product-description"));
 app.use("/api/product-features", require("./router/product-feature"));
-app.use("/api/product-usedby", require("./router/product-used-by"));
+app.use("/api/product-used-by", require("./router/product-used-by"));
 app.use("/api/banners", require("./router/banner"));
 app.use("/api/categories", require("./router/category"));
 app.use("/api/sub-categories", require("./router/sub-category"));
