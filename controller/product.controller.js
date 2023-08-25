678d63bf7d62b41f2277a9737fb809a0266cc8f7
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-08-25 12:59:30 +0530

    added multiple feature creation

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -56,9 +56,9 @@ async function deleteProductById(req, res) {
     if (productExist.rowCount === 0)
       return res.status(404).json({ message: "Product not found!" });
 
-    console.log(productExist);
+    console.log(productExist.rows[0].images);
 
-    productExist.images.forEach((imagePath) => {
+    productExist.rows[0].images.forEach((imagePath) => {
       const fileName = path.basename(imagePath);
       const filePath = path.join(__dirname, "../assets/products", fileName);
       fs.unlinkSync(filePath);
