ca9a7a1a9e1285dd689665cee6c1b7ba3b8b68c5
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-08-27 19:06:33 +0530

    added recently viewed products in users and product queries

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
