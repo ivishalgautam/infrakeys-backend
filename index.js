d6349e7dc782860249f43ca1a05294ef7e18d7ed
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-02 11:39:38 +0530

    added cors

--- index.js
+++ index.js
@@ -16,17 +16,20 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "http://62.72.59.14/",
-    "http://127.0.0.1:7000/"
-  );
+  // res.header("Access-Control-Allow-Origin : *");
   // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:7000/");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization,Access-Control-Allow-Origin"
-  );
+  // res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Content-Type, Authorization,Access-Control-Allow-Origin"
+  // );
+
+  // res.header("Content-Type: application/json");
+  res.header("Access-Control-Allow-Origin: *");
+  res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+  // res.header(
+  //   "Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization"
+  // );
   next();
 });
 
