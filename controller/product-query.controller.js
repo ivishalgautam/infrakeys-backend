a98ca6d659f6efac4c513f14bdd0bef874ec1d5f
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-04 16:43:05 +0530

    added auth middlewares

--- controller/product-query.controller.js
+++ controller/product-query.controller.js
@@ -66,7 +66,7 @@ async function getAllQueries(req, res) {
     const { rows } = await pool.query(
       `SELECT
           pq.id AS query_id,
-          p.name,
+          p.title,
           u.name as user_name,
           pq.timestamp
       FROM
