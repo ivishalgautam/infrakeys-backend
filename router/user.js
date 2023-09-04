aec6527792bd9df12f92769d14b019474c2ce2fa
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-04 10:46:33 +0530

    added auth middlewares

--- router/user.js
+++ router/user.js
@@ -5,12 +5,13 @@ const {
   deleteUserById,
   getUserById,
 } = require("../controller/user.controller");
+const { verifyTokenAndAuthorization } = require("../middleware/verifyToken");
 
 // routes
-router.get("/", getUsers);
-router.put("/:userId", updateUserById);
-router.delete("/:userId", deleteUserById);
-router.get("/:userId", getUserById);
+router.get("/", verifyTokenAndAuthorization, getUsers);
+router.put("/:userId", verifyTokenAndAuthorization, updateUserById);
+router.delete("/:userId", verifyTokenAndAuthorization, deleteUserById);
+router.get("/:userId", verifyTokenAndAuthorization, getUserById);
 // router.get("/", getProducts);
 
 module.exports = router;
