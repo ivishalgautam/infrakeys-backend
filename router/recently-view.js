11f95008c5a98231818f594b1e5e893f88fb43c0
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-04 17:40:40 +0530

    added auth middlewares

--- router/recently-view.js
+++ router/recently-view.js
@@ -2,11 +2,19 @@ const {
   viewProduct,
   recentlyViewed,
 } = require("../controller/recently-view.controller");
-const { isLoggedIn } = require("../middleware/verifyToken");
+const {
+  isLoggedIn,
+  verifyTokenAndAuthorization,
+} = require("../middleware/verifyToken");
 
 const router = require("express").Router();
 
 router.post("/view-product", isLoggedIn, viewProduct);
 router.get("/recently-viewed/:userId", isLoggedIn, recentlyViewed);
+router.get(
+  "/admin/recently-viewed/:userId",
+  verifyTokenAndAuthorization,
+  recentlyViewed
+);
 
 module.exports = router;
